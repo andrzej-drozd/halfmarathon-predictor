50ecf01e4957cc0e66a412807cd87b7383f89bc4
Andrew <andrzej.drozd@gmail.com>
2025-12-24 15:55:11 +0100

    Read ALLOWED_HOSTS from env

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,10 @@ SECRET_KEY = 'django-insecure-3kivxu77x81hg#ms%f7-rnr*rcl65llm^qz)ns2dzg)pb@mq=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [h.strip() for h in ALLOWED_HOSTS if h.strip()]
+
 
 
 # Application definition
